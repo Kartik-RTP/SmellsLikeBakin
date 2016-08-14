1921bd56ea5b2c80fe38bfa373db3d2a93db3e65
kartik <kartik.gupta@iitrpr.ac.in>
2016-08-15 01:48:09 +0530

    Added code for ListFragment

--- app/src/main/java/com/kartikgupta/smellslikebakin/MainActivity.java
+++ app/src/main/java/com/kartikgupta/smellslikebakin/MainActivity.java
@@ -1,13 +1,29 @@
 package com.kartikgupta.smellslikebakin;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements ListFragment.OnRecipeSelectedInterface{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ListFragment listFragment = new ListFragment();
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.placeholder,listFragment);
+        fragmentTransaction.commit();
+
+    }
+
+    @Override
+    public void onListRecipeSelected(int index) {
+        Toast.makeText(MainActivity.this, Recipes.names[index], Toast.LENGTH_SHORT).show();
     }
 }
